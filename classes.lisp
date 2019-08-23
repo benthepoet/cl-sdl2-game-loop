08378544c344dfa0c62e8fb6ef42c0902d4d5698
Ben Hanna <benpaulhanna@gmail.com>
2019-08-23 13:03:18 -0400

    Cleaned up animation class and routines.

--- classes.lisp
+++ classes.lisp
@@ -1,24 +1,24 @@
 (in-package :cl-sdl2-game-loop)
 
 (defclass animation ()
-  ((x
-    :initarg :x)
-   (y
-    :initarg :y)
-   (w
-    :initarg :w)
-   (h
-    :initarg :h)
-   (total
-    :initarg :total)
-   (duration
-    :initarg :duration)
+  ((name
+    :initarg :name)
+   (frame-width
+    :initarg :frame-width)
+   (frame-height
+    :initarg :frame-height)
+   (frame-total
+    :initarg :frame-total)
+   (frame-duration
+    :initarg :frame-duration)
+   (repeat
+    :initarg :repeat)
    (texture-path
     :initarg :texture-path
     :accessor texture-path)
-   (current
+   (frame-count
     :initform 0)
-   (timer
+   (frame-timer
     :initform 0)))
 
 (defclass sprite ()
@@ -37,14 +37,14 @@
     :initform (make-hash-table :test #'equal)
     :accessor textures)))
 
-(defun make-animation (x y w h total duration texture-path)
+(defun make-animation (name frame-width frame-height frame-total frame-duration repeat texture-path)
   (make-instance 'animation
-                 :x x
-                 :y y
-                 :w w
-                 :h h
-                 :total total
-                 :duration duration
+                 :name name
+                 :frame-width frame-width
+                 :frame-height frame-height
+                 :frame-total frame-total
+                 :frame-duration frame-duration
+                 :repeat repeat
                  :texture-path texture-path))
 
 (defun make-sprite (animation)
@@ -56,5 +56,5 @@
                  :renderer renderer
                  :sprites (list
                            (make-sprite
-                            (make-animation 0 0 24 24 4 10 "assets/run.bmp")))))
+                            (make-animation "run" 24 24 4 10 t "assets/run.bmp")))))
 
